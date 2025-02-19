6b683fdd98ba4df2356c91486f3d0b1444cd9fa2
Paul-Alejandro <jandroaraujo@gmail.com>
2025-02-19 14:12:05 +0100

    Nuevo commit: corrección de código -> Pylint 10/10

--- src/funciones.py
+++ src/funciones.py
@@ -1,39 +1,24 @@
-
-'''
-Paso 1 - funcion_factorial_1_pasa
+"""Módulo que contiene la función factorial
+La función factorial calcula el factorial de un número entero positivo."""
 def factorial(n):
-    return 1  # Retornamos 1 directamente solo para que pase el test
+    """Calcula el factorial de un número n.
 
-'''
+    Parámetros:
+        n (int): Número entero no negativo.
 
-'''
-Paso 2 : para el tipo de dato
+    Levanta:
+        TypeError: Si n no es un entero.
+        ValueError: Si n es negativo.
 
-def factorial(n):
-    if not isinstance(n, int):
-        raise TypeError("El número debe ser un entero")
-    return 1  # Retornamos 1 directamente solo para que pase el test
-'''
-'''
-Paso 3 : para números negativos
-
-def factorial(n):
+    Retorna:
+        int: Factorial de n
+    """
     if not isinstance(n, int):
         raise TypeError("El número debe ser un entero")
     if n < 0:
         raise ValueError("El número debe ser positivo")
-    return 1  # Retornamos 1 directamente solo para que pase el test
-
-'''
-
-def factorial(n):
-    if not isinstance(n, int):
-        raise TypeError("El número debe ser un entero")
-    if n < 0:
-        raise ValueError("El número debe ser positivo")
-    if n == 0 or n == 1:
+    if n in (0,1):
         return 1
-    
     resultado = 1
     for i in range(2, n + 1):
         resultado *= i
